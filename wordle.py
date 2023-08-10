a176361e430216d8bbd760435b0394ddca3538e5
JacksonBoswell <141806682+JacksonBoswell@users.noreply.github.com>
2023-08-10 17:56:40 -0500

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -1,18 +1,3 @@
-
-'''
-
-The ai bot and the most optimal guess is based on the expected bit rate of a guess
-A word can be the secret word but still not be a wise or optimal guess 
--- this is why a correct answer may be in a low percentile
-
-If a word is sub-optimal, meaning that it is not in the narrowed down word list, the expected bit rate cannot be found
---instead the actual bit rate is used
-
-
-'''
-
-
-
 import random
 import math
 
